84225af913ac52f81cf9dd4a5d11604d6f549a94
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-04 12:36:56 +0300

    map

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,6 +8,7 @@ function Card(props) {
     return (
         <div className="card">
             <div className="top">
+                <p>{props.id}</p>
                 <h2 className="name">{props.name}</h2>
                 <Avatar  src={props.img}/>
             </div>
