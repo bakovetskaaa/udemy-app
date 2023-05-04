26bab0aadc0270c0a7a2dfc75831755d411f1a43
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-04 14:11:03 +0300

    map-practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-function App(){
-    return (<div>
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
 
+function createEntry(emoji){
+    return (
+        <Entry
+        key = {emoji.id}
+        id={emoji.id}
+        emoji={emoji.emoji}
+        name={emoji.name}
+        meaning={emoji.meaning}
 
-    </div>)
-};
+        />
+    )
+}
+
+function App() {
+    return (
+        <div>
+            <h1>
+                <span>emojipedia</span>
+            </h1>
+                {emojipedia.map(createEntry)}
+
+        </div>
+    );
+}
 
 export default App;
