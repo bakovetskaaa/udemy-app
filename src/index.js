294f4d5a483f694fdfe3ad19120e906bd4c58d62
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-07 15:21:05 +0300

    map_filter_reduce

--- src/index.js
+++ src/index.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// var numbers = [3, 56, 2, 48, 5];
+//
+// //Map -Create a new array by doing something with each item in an array.
+// // function double(x){
+// //     return x * 2;
+// // }
+// //
+// // const newNumbers = numbers.map(double);
+//
+// // var newNumbers = [];
+// // numbers.forEach(function (x){
+// //     newNumbers.push(x * 2)
+// // });
+//
+// // var newNumbers = numbers.map(function (x) {
+// //     return (x * 2)
+// // })
+// //
+// // console.log(newNumbers);
+//-----------------------------------------------------------------------------------------------//
+// //Filter - Create a new array by keeping the items that return true.
+//
+//
+// // const newNumbers = numbers.filter(function(num) {
+// //     return num < 10;
+// // })
+//
+// // var newNumbers = [];
+// // numbers.forEach(function (num){
+// //     if (num < 10){
+// //         newNumbers.push(num)
+// //     }
+// // })
+//
+// // console.log(newNumbers);
+//-----------------------------------------------------------------------------------------------//
+// //Reduce - Accumulate a value by doing something to each item in an array.
+//
+// // var newNumbers = numbers.reduce(function (accumulator, currentNumber){
+// //     return accumulator + currentNumber
+// // })
+//
+// // var newNumbers= 0;
+// // numbers.forEach(function (currentNumber){
+// //     newNumbers += currentNumber
+// // })
+// //
+// // console.log(newNumbers);
+//-----------------------------------------------------------------------------------------------//
+// //Find - find the first item that matches from an array.
+//
+// // var newNumber = numbers.find(function (num){
+// //     return num > 10;
+// // })
+// //
+// // console.log(newNumber);
+//
+//-----------------------------------------------------------------------------------------------//
+// //FindIndex - find the index of the first item that matches.
+// var newNumber = numbers.findIndex(function (num){
+//     return num > 10;
+// })
+//
+// console.log(newNumber);
 
-import App from './components/App';
+import emojipedia from "./emojipedia";
 
-    ReactDOM.render(
-        <div>
-            <App/>
-        </div>,
-        document.getElementById("root"));
+console.log(emojipedia);
 
+var string = emojipedia.map(function (x){
+    return x.meaning.slice(0, 100)
+})
 
-//----//1. Create a new React app.
-//----//2. Create a App.jsx component.
-//----//3. Create a Header.jsx component that renders a <header> element
-//----//to show the Keeper App name in an <h1>.
-//----//4. Create a Footer.jsx component that renders a <footer> element
-//----//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to apply styling.
+console.log(string);
