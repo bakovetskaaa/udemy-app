ce444a72cb1913cdfb5069c358b9d62e2b1dbf89
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-09 14:14:05 +0300

    arrow functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-function App(){
-    return (<div>
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
 
-    </div>)
-};
+
+function App() {
+    return (
+        <div>
+            <h1>
+                <span>emojipedia</span>
+            </h1>
+            <dl className="dictionary">{emojipedia.map(emojiTerm =>
+                    <Entry
+                        key={emojiTerm.id}
+                        emoji={emojiTerm.emoji}
+                        name={emojiTerm.name}
+                        description={emojiTerm.meaning}
+                    />
+            )}
+            </dl>
+        </div>
+    );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import App from './components/App';
+ReactDOM.render(<App />, document.getElementById("root"));
 
-    ReactDOM.render(
-        <div>
-            <App/>
-        </div>,
-        document.getElementById("root"));
+var numbers = [3, 56, 2, 48, 5];
+
+// const newNumbers = numbers.map( x => x * x)
+
+//Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(x=> x * 2);
 
 
-//----//1. Create a new React app.
-//----//2. Create a App.jsx component.
-//----//3. Create a Header.jsx component that renders a <header> element
-//----//to show the Keeper App name in an <h1>.
-//----//4. Create a Footer.jsx component that renders a <footer> element
-//----//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(num => num < 10);
 
-//HINT: You will need to study the classes in teh styles.css file to apply styling.
+//Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber)
+
+//Find - find the first item that matches from an array.
+// const newNumber = numbers.find(num => num > 10);
+
+//FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex( num => num > 10);
+// console.log(newNumber);
+
