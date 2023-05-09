320eda5e2a34eb5fb79d7825b3c194b0e76e0d34
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-09 16:30:57 +0300

    keeper app 0.2

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            <div>{notes.map(notesTerm =>
+                <Note
+                    key={notesTerm.key}
+                    title={notesTerm.title}
+                    content={notesTerm.content}
+                />
+                )
+            }
+                </div>
             <Footer />
         </div>
     );
