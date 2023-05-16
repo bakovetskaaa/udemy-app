6058af1c4e4601682a51205ec4324c716e2cb8e8
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-16 12:52:38 +0300

    handing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {useState} from "react";
 
-function App(){
-    return (<div>
+function App() {
+    const [headingText, setHeadingText] = useState("Hello!");
+    const [isMouseOver, setMouseOver] =useState(false);
 
+    function handleClick() {
+    setHeadingText("Submitted");
+    }
 
-    </div>)
-};
+    function handleMouseOver(x) {
+        setMouseOver(true);
+    }
+    function handleMouseOut(x) {
+        setMouseOver(false);
+    }
+
+    return (
+        <div className="container">
+            <h1>{headingText}</h1>
+            <input type="text" placeholder="What's your name?" />
+            <button
+                style={{backgroundColor: isMouseOver ? "black" :  "white" }}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                onClick={handleClick}>
+                Submit
+            </button>
+        </div>
+    );
+}
 
 export default App;
