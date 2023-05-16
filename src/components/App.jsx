6d89fd46ceec64a857c0ab54c76ede6d1f5ecb72
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-16 22:58:00 +0300

    changing complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,68 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {useState} from "react";
 
-function App(){
-    return (<div>
+function App() {
+     // const [name, setName] = useState("");
+     // const [surname, setSurname] = useState("");
+
+     const [fullName, setFullName] = useState({
+         fName: "",
+         lName: ""
+
+     });
+
+     // function handleName(event) {
+     //     setName(event.target.value)
+     // }
+     //
+     // function handleSurname(event) {
+     //     setSurname(event.target.value)
+     // }
+
+    function handleChange(event){
+
+        const { value, name } = event.target;
+        //
+        // console.log(value);
+        // console.log(name);
+
+        setFullName(prevValue => {
+            if(name === "fName"){
+                return {
+                    fName: value,
+                    lName: prevValue.lName
+                };
+            } else if (name === "lName"){
+                return {
+                    fName: prevValue.fName,
+                    lName: value
+                }
+            }
+        })
+    }
 
 
-    </div>)
-};
+
+    return (
+        <div className="container">
+            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <form>
+
+                <input
+                    onChange={handleChange}
+                    name="fName"
+                    placeholder="First Name"
+                    value={fullName.fName}/>
+
+                <input
+                    onChange={handleChange}
+                    name="lName"
+                    placeholder="Last Name"
+                    value={fullName.lName}/>
+
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
 
 export default App;
