2574f0d8ea00369e3ffd1ccf1b64cd26399b3146
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-17 15:21:25 +0300

    changing practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
-     // const [name, setName] = useState("");
-     // const [surname, setSurname] = useState("");
+    const [contact, setContact] = useState({
+        fName: "",
+        lName: "",
+        email: ""
+    });
 
-     const [fullName, setFullName] = useState({
-         fName: "",
-         lName: ""
+    function handleChange(event) {
+        const {value, name} = event.target;
 
-     });
-
-     // function handleName(event) {
-     //     setName(event.target.value)
-     // }
-     //
-     // function handleSurname(event) {
-     //     setSurname(event.target.value)
-     // }
-
-    function handleChange(event){
-
-        const { value, name } = event.target;
-        //
-        // console.log(value);
-        // console.log(name);
-
-        setFullName(prevValue => {
-            if(name === "fName"){
+        setContact(prevValue =>{
+            if (name === "fName"){
                 return {
                     fName: value,
-                    lName: prevValue.lName
-                };
+                    lName: prevValue.lName,
+                    email: prevValue.email
+
+                }
             } else if (name === "lName"){
                 return {
                     fName: prevValue.fName,
-                    lName: value
+                    lName: value,
+                    email: prevValue.email
+                }
+            } else if ( name === "email"){
+                return {
+                    fName: prevValue.fName,
+                    lName: prevValue.lName,
+                    email: value
                 }
             }
         })
-    }
 
+    }
 
 
     return (
         <div className="container">
-            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <h1>
+                Hello {contact.fName} {contact.lName}
+            </h1>
+            <p>{contact.email}</p>
             <form>
-
-                <input
-                    onChange={handleChange}
-                    name="fName"
-                    placeholder="First Name"
-                    value={fullName.fName}/>
-
-                <input
-                    onChange={handleChange}
-                    name="lName"
-                    placeholder="Last Name"
-                    value={fullName.lName}/>
-
+                <input onChange={handleChange} name="fName" placeholder="First Name" value={contact.fName}/>
+                <input onChange={handleChange} name="lName" placeholder="Last Name" value={contact.lName}/>
+                <input onChange={handleChange} name="email" placeholder="Email" value={contact.email}/>
                 <button>Submit</button>
             </form>
         </div>
