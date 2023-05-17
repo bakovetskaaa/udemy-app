32828b283e958b137ea119030738bf4fc9b61739
bakovetskaaa <olyabakovetska@gmail.com>
2023-05-17 17:22:45 +0300

    spread practice

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import App from './components/App';
+ReactDOM.render(<App />, document.getElementById("root"));
 
-    ReactDOM.render(
-        <div>
-            <App/>
-        </div>,
-        document.getElementById("root"));
-
-
-//----//1. Create a new React app.
-//----//2. Create a App.jsx component.
-//----//3. Create a Header.jsx component that renders a <header> element
-//----//to show the Keeper App name in an <h1>.
-//----//4. Create a Footer.jsx component that renders a <footer> element
-//----//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to apply styling.
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
